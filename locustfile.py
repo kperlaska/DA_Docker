71810e3002cab66a238d549d4169145e7b6d18cf
Kristjan Perlaska <kristjan.perlaska@swisscom.com>
2017-07-14 15:00:04 +0200

    removing some urls

--- locustfile.py
+++ locustfile.py
@@ -7,12 +7,6 @@ def global_en_home(l):
     urls.add('/global/en/home/')
     urls.add('/global/en/investment-excellence/research/')
     urls.add('/global/en/discover-julius-baer/corporate-information/facts-figures/')
-    urls.add('/group/en/financial-information/financial-reporting/')
-    urls.add('/global/en/investment-excellence/research/cio-house-view/')
-    urls.add('/global/en/visionary-thinking/fia-formula-e-championship/video-gallery/')
-    urls.add('/global/en/menus/footer/rss-feed/')
-    urls.add('/group/en/investors/')
-    urls.add('/financial-information/key-figures/')
 
 
     headers = {
