3c456973d92fa1fc79ca60501a8220c421d36125
Kristjan Perlaska <kristjan.perlaska@swisscom.com>
2017-07-17 20:03:37 +0200

    adding use case

--- locustfile.py
+++ locustfile.py
@@ -4,18 +4,12 @@ import random
 
 def global_en_home(l):
     urls = set()
-    urls.add('/global/en/home/')
-    urls.add('/global/en/investment-excellence/research/')
-    urls.add('/global/en/discover-julius-baer/corporate-information/facts-figures/')
-    urls.add('/group/en/financial-information/financial-reporting/')
-    urls.add('/group/en/menus/services/contacts/')
-    urls.add('/global/en/services-solutions/open-product-service-platform/wealth-planning/')
-    urls.add('/global/en/investment-excellence/research/cio-house-view/')
-    urls.add('/global/en/visionary-thinking/fia-formula-e-championship/video-gallery/')
-    urls.add('/global/en/menus/footer/rss-feed/')
-    urls.add('/group/en/investors/')
-    urls.add('/group/en/financial-information/key-figures/')
-
+    urls.add('/global/en/discover-julius-baer/gen-y/gen-y-investment-workshop/')
+    urls.add('/careers/en/home/')
+    urls.add('/careers/en/experienced-professionals/why-work-with-us/employee-benefits/')
+    urls.add('/careers/en/apprentices-in-switzerland/applying/instructions-for-a-successful-application/')
+    urls.add('/global/en/investment-excellence/research/wealth-report-europe/')
+    urls.add('/index.php?eID=dumpFile&t=f&f=12188&token=59c3feae750525339725045e56e65147b194be8a')
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
